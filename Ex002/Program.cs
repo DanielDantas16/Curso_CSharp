e5e07343feeef4f3f205210e04b384a568799f2c
Daniel <danielguimazinho@gmail.com>
2025-01-13 22:18:38 -0300

    Adicionar arquivos de projeto.

---
+++ Ex002/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = -45;
+            float num2 = 4.58f;
+            char letra = 'c';
+            string nome = "Daniel";
+            Console.WriteLine("O número é {0}.", num1);
+            Console.WriteLine("O valor é R${0}." ,num2);
+            Console.WriteLine("a letra é {0}. ",letra);
+            Console.WriteLine("Seu nome é {0}.",nome);
+            Console.WriteLine("O número é {0}, " + "o valor é R$ {1}" + " a letra é {2} " + "seu nome é {3}.", num1, num2, letra, nome);
+        }
+
+    }
+}
---
+++ Ex003/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ex003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            float resposta;
+
+            num1 = 45;
+            num2 = 35;
+            num3 = num1 - num2;
+            Console.WriteLine("A subtração de {0} - {1} = {2}", num1, num2, num3);
+
+            num3 = num1 + num2;
+            Console.WriteLine("A adição de {0} + {1} = {2}", num1, num2, num3);
+
+            num3 = num1 * num2;
+            Console.WriteLine("A multiplicação de {0} * {1} = {2}", num1, num2, num3);
+
+            resposta = num1 / num2;
+            Console.WriteLine("A divisão de {0} / {1} = {2}", num1, num2, resposta);
+
+            resposta = num1 % num2;
+            Console.WriteLine("O resto da divisão de {0} % {1} = {2}", num1, num2, resposta);
+
+            float num4 = 35f;
+            resposta = num1 / num4;
+
+            Console.WriteLine("A divisão de {0} / {1} = {2}", num1, num4, resposta);
+        }
+    }
+}
