eeb95d04d1945521bd0dde37b613a59e830f18d6
Daniel <danielguimazinho@gmail.com>
2025-01-15 21:25:24 -0300

    Novos exercicios

---
+++ Ex007/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ex007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double nota_final;
+            string resultado;
+
+            Console.WriteLine("Digite a nota final do aluno: ");
+            nota_final = Convert.ToDouble(Console.ReadLine());
+
+            if (nota_final >= 70)
+            {
+                resultado = "Aprovado";
+                if (nota_final >= 95)
+                {
+                    resultado = "Aprovado com louvor";
+                }
+            }
+            else if (nota_final >= 45)
+            {
+                resultado = "Recuperação";
+            }
+            else
+            {
+                resultado = "Reprovado";
+            }
+            Console.WriteLine("O aluno ficou {1}.", nota_final, resultado);
+        }
+    }
+}
