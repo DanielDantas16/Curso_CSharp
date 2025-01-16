f27d4ea946b4b55f3784496dfc253ddf1e3eac3d
Daniel <danielguimazinho@gmail.com>
2025-01-15 22:21:31 -0300

    Exercicios novos

---
+++ Ex008/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicios008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a velocidade percorrida: ");
+            double velocidade = Convert.ToDouble(Console.ReadLine());
+
+            if (velocidade > 80)
+            {
+                double multa = (velocidade - 80) * 7.0;
+                Console.WriteLine("Você foi mutado em R${0} " , multa);
+            }
+            else
+            {
+                Console.WriteLine("Você esta na velocidade adequada, parabéns!");
+            }
+        }
+    }
+}
---
+++ Ex009/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex009
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero inteiro: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string resultado;
+
+            if (num % 2 == 0)
+            {
+                resultado = ("O número digitado {0} ele é par ");
+            }
+            else
+            {
+                resultado = (" número digitado {0} ele é impar ");
+            }
+            Console.WriteLine(resultado, num);
+        }
+    }
+}
