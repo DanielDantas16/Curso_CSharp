6a87c23c124deefe81ef7736a82b516f4bd849a8
Daniel <danielguimazinho@gmail.com>
2025-01-20 19:41:37 -0300

    Exercicio do teste de Triangulo

---
+++ Ex006/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite um número: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Ele é par");
+
+            }
+            else
+            {
+                Console.WriteLine("Ele é impar");
+            }
+        }
+    }
+}
