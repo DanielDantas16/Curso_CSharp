f12d1e51739a27ae67e5b7cd3210aa4bfff70b48
Daniel <danielguimazinho@gmail.com>
2025-01-20 21:55:01 -0300

    Exercicios de salário!

---
+++ Ex11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.Design;
+
+namespace Ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva o seu salário: ");
+            double salário = Convert.ToDouble(Console.ReadLine());
+            double aumento;
+
+            if (salário > 2500)
+            {
+                aumento = salário * 0.5;
+                Console.WriteLine("O seu salário com aumento agora é {0}: ", (aumento + salário));
+
+
+            }
+            else if (salário > 1250)
+            {
+                aumento = salário * 0.10;
+                Console.WriteLine("O seu salário com aumento agora é {0}: ", (aumento + salário));
+            }
+            else
+            {
+                aumento = salário * 0.15;
+                Console.WriteLine("O seu salário com aumento agora é {0}: ", (aumento + salário));
+            }
+        }
+    }
+}
