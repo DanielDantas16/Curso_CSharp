183cbc75801c15c65005c6552bbaf5d08f541078
Daniel <danielguimazinho@gmail.com>
2025-01-22 20:09:24 -0300

    Exercicios for

---
+++ Ex012/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ex012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++) { 
+            Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Ex12/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Contagem regressiva!");
+            Console.WriteLine("----------------------------");
+
+            for (int i = 10; i > 0; i--) 
+            {
+
+                Console.WriteLine("{0}, ",i);
+            }
+        }
+    }
+}
+
