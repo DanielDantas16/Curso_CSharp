5df468f37e313f37c80e014aa08f669bc1e1cfa1
Daniel <danielguimazinho@gmail.com>
2025-01-22 22:00:17 -0300

    Ex de While

---
+++ Ex15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Ex15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String genero;
+            Console.WriteLine("Digite seu genero: ");
+            genero = Console.ReadLine().ToUpper();
+           
+            while (genero != "F" && genero != "M")
+               
+            {
+                Console.WriteLine("Digite seu genero");
+                genero = Console.ReadLine().ToUpper();
+            }
+            Console.WriteLine("Bem-vindos e Bem-vindas ao curso de C#!");
+
+        }    }
+}
