d2b51d7246fc24d0743b9543ed98200e06feb06a
Daniel <danielguimazinho@gmail.com>
2025-01-27 18:57:45 -0300

    Do while

---
+++ Ex16/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 5;
+
+         /*   while (num < 5)
+            {
+                Console.WriteLine("Curso C#");
+            } */
+         
+
+            do
+            {
+                Console.WriteLine("Curso C#");
+            }while (num < 5);
+        }
+    }
+}
