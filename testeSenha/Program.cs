b8e7d4b8d6dc9d1f170f3fc51f470d9c211dc6af
Daniel <danielguimazinho@gmail.com>
2025-01-27 19:29:46 -0300

    Programa de senha

---
+++ testeSenha/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+
+namespace testeSenha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String senha = "123";
+            String senhaDigitada;
+            int tentativas = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a senha: ");
+                senhaDigitada = Console.ReadLine();
+                tentativas++;
+                if (tentativas > 3)
+                { break; }
+
+            }while (senha != senhaDigitada);
+
+            if ( senha != senhaDigitada ) {
+            Console.Clear() ;
+            Console.WriteLine("Senha incorreta! Tentativas: " + tentativas);
+
+                }else
+            {
+                Console.Clear();
+                Console.WriteLine("Senha correta! Tentativas: " + tentativas);
+            }
+                }
+    }
+}
