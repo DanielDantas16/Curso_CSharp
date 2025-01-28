df2f76d01904885f593af0154dbdbde2f23e0638
Daniel <danielguimazinho@gmail.com>
2025-01-27 21:33:42 -0300

    Datas

---
+++ datas/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace datas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meses = { "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", " Novembro", "Dezembro" };
+            int[] dia = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            for (int i = 0; i < meses.Length; i++)
+            {
+                Console.WriteLine("O mês de " + meses[i] + " tem " + dia[i] + " dias.");
+            }
+            string[] semana = { "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado" };
+            foreach (string dias in semana)
+            {
+                Console.WriteLine(dias);
+            }
+            Console.WriteLine();
+            foreach (string mes in meses)
+            {
+                Console.WriteLine(mes);
+            }
+        }
+    }
+}
