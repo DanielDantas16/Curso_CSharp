3fb9a07d136146dc01cb2f3d0cbc249c5942e4a6
Daniel <danielguimazinho@gmail.com>
2025-01-29 21:29:37 -0300

    Calculo do triângulo

---
+++ CalcularAreadoTriangulo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CalcularAreadoTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            triangulo(6,9);
+        }
+        static void triangulo(double area, double altura) {
+
+            double baseT = (area * altura)/2;
+            Console.WriteLine("O resultado do calculo do triângulo é de: " + baseT);
+
+
+    } }
+}
