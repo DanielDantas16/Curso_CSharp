d0716256c1a734999daf6eb4a23ce6cafe3c8a3c
Daniel <danielguimazinho@gmail.com>
2025-02-03 20:54:34 -0300

    Sistema de voto

---
+++ Voto/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Voto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nasc;
+            Console.WriteLine("Digite o ano de nascimento: ");
+            nasc = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(voto(nasc));
+        }
+        static string voto(int ano)
+        {
+            DateTime dataAtual = DateTime.Now;
+            int anoAtual = dataAtual.Year;
+            int idade = anoAtual - ano;
+
+            if (idade < 16)
+            {
+                return $"Com {idade} anos, você não vota.";
+            }
+            else if (idade > 18 && idade < 70)
+            {
+                return $"Com {idade} anos, você vota.";
+
+            }
+            else
+            {
+                return $"Com {idade} ";
+            }
+        }
+    }
+}
---
+++ calculadora/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            operacao();
+
+        }
+
+        static void operacao()
+        {
+
+            Console.WriteLine("Informe um número para fazer o calculo: ");
+            double adicao1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe outro número para fazer o calculo: ");
+            double adicao2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Qual operação você deseja fazer? +, -, *, /");
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
